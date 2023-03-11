2c205099c42b93d817a9aac7b7ffeb7af169bfe7
Vipul Jain <75699731+vj015@users.noreply.github.com>
2023-03-12 00:34:10 +0530

    added bootstrap and icons

--- ngofrontend/src/views/AboutView.vue
+++ ngofrontend/src/views/AboutView.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
     <h1>This is an about page</h1>
+    <font-awesome-icon icon="fa-solid fa-user-secret" />
   </div>
 </template>
--- ngofrontend/src/views/HomeView.vue
+++ ngofrontend/src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
+  <div class="container">
     <h1>{{ "Home Page" }}</h1>
+    <b-button variant="outline-primary">Primary</b-button>
   </div>
 </template>
 
