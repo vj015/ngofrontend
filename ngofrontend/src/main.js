13c8fe4efdf5a68d71226bcd48d05d7913233c5a
Vipul Jain <75699731+vj015@users.noreply.github.com>
2023-03-12 00:36:08 +0530

    Revert "Revert "added bootstrap and icons""
    This reverts commit e4fc9fcd67d70115ca3b52ca61e97ba9c131ce4b.

--- ngofrontend/src/main.js
+++ ngofrontend/src/main.js
@@ -2,8 +2,26 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+
+/* import the fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+/* import specific icons */
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
+
+/* add icons to the library */
+library.add(faUserSecret);
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 Vue.config.productionTip = false;
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 
 new Vue({
   router,
